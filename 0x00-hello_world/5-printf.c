c450ca451978f69e54abf14e45b06c1a016347f2
UHRAI <hajarerrmo@gmail.com>
2023-04-20 16:38:06 -0700

    5-ptintf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main -entry
- * return: always 0
+ * main - Entry
+ * Return: Always 0
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
