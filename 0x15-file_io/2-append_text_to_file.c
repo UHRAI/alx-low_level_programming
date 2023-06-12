f76f63988bed0f2a2cbf3e54b2290a427bb8e984
UHRAI <hajarerrmo@gmail.com>
2023-06-12 15:35:04 +0200

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	opn = open(filename, O_WRONLY | O_APPEND);
 	wr = write(opn, text_content, len);
 
-	if (opn == -1 || w == -1)
+	if (opn == -1 || wr == -1)
 		return (-1);
 
 	close(opn);
